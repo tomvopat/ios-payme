4e1c1863a896ac700dc24791ac6a120f226d364f
Tomáš Vopat <tomas.vopat@gmail.com>
2019-02-10 09:57:58 +0100

    Creating project + adding Podfile

---
+++ PayMe/PayMe/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PayMe
+//
+//  Created by Tomáš Vopat on 10.F.19.
+//  Copyright © 2019 Tomáš Vopat. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ PayMe/Pods/Target Support Files/Pods-PayMe/Pods-PayMe-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_PayMe : NSObject
+@end
+@implementation PodsDummy_Pods_PayMe
+@end
